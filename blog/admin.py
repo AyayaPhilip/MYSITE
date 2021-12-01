fef11dea44fb80f21daab13989327ff7a904e1fd
phanay <philipayaya3@gmail.com>
2021-12-01 07:41:14 +0300

    edited the templates folder

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_display = ('title', 'author', 'slug', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
